add94e7aaabe224dfb366c9d877d161a554883d9
Thaddeus Osalvo <57509048+tadosalvo@users.noreply.github.com>
2020-12-22 16:14:07 -0600

    Add files via upload

---
+++ OsalvoHelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author	Thaddeus Osalvo
+ * Date		August 24, 2020
+ * Purpose	To say Hello to the entire world
+ *
+ */
+public class OsalvoHelloWorld {
+
+	public static void main(String[] args) {
+		// This statement prints Hello World to the console
+		System.out.println("Hello World");
+		System.out.print("My name is ");
+		System.out.print("Thaddeus");
+	}
+
+}
